ef62f55de6098a2a152196d6234b836c187b167d
Nora <nora5dempsey@gmail.com>
2021-08-09 09:00:04 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+newHeader.innerHTML = "<h1> Nora is the champion </h1>"
